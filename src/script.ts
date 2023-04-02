6fd28861878c5c58bab65ec3b3ffea71bb6a1a3a
Connor <ces156@pitt.edu>
2023-04-01 22:27:49 -0400

    Will write different games to different sheets. Have problem with trying to loop to game 8775

--- src/script.ts
+++ src/script.ts
@@ -5,15 +5,16 @@ import GameData from '../model/GameModel'
 import * as path from 'path';
 import GameModel from '../model/GameModel';
 
-for (let i = 0; i < 10; i++) {
-    const url = `https://cdl-other-services.abe-arsfutura.com/production/v2/content-types/match-detail/bltd79e337aca601012?locale=en-us&options=%7B%22id%22%3A${i + 8760}%7D`
+const workbook = new Excel.Workbook()
+
+for (let i = 0; i < 24; i++) {
+    const url = `https://cdl-other-services.abe-arsfutura.com/production/v2/content-types/match-detail/bltd79e337aca601012?locale=en-us&options=%7B%22id%22%3A${8750 + i}%7D`
 
     const data = getData(url).then(data => {
-        if (data.data.matchData.matchExtended.match.status !== 'completed') {
+        if (data.data.matchData.matchExtended.match.status !== 'COMPLETED') {
             return;
         }
         const homeTeam = setTeamData(data.data.matchData.matchExtended.homeTeamCard) //get home team
-        console.log(data.data.matchData.matchStats)
         const homePlayers = setPlayerData(data.data.matchData.matchStats.overall.hostTeam) //get home players
         const awayTeam = setTeamData(data.data.matchData.matchExtended.awayTeamCard) //get away team
         const awayPlayers = setPlayerData(data.data.matchData.matchStats.overall.guestTeam) //get away players
@@ -23,7 +24,7 @@ for (let i = 0; i < 10; i++) {
 
         const game = setGameData(data.data.matchData.matchExtended, homeTeam, awayTeam)
 
-        writeData([game])
+        writeData([game], 8750 + i)
     })
 }
 
@@ -78,10 +79,8 @@ for (let i = 0; i < 10; i++) {
     }
 
     //Write to excel
-    async function writeData(data: any) {
-
-        const workbook = new Excel.Workbook()
-        const worksheet = workbook.addWorksheet('My Sheet')
+    async function writeData(data: any, gameId: number) {
+        const worksheet = workbook.addWorksheet("Game " + gameId)
         worksheet.columns = [
             { header: 'GameId', key: 'id', width: 10 },
             { header: 'Team', key: 'teamName', width: 20 },
@@ -124,7 +123,7 @@ for (let i = 0; i < 10; i++) {
                 })
             })
         })
-        const exportPath = path.resolve(__dirname, 'countries.xlsx');
+        const exportPath = path.resolve(__dirname, 'gameStats.xlsx');
         await workbook.xlsx.writeFile(exportPath);
         
     }
